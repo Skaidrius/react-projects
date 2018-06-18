54bf3dfd37767aa761d42092f3815992061a8860
Skaidrius <skaidrius@gmail.com>
2018-06-18 14:47:53 +0000

    start simple React todo app

---
+++ react/simple-todo-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+
+  }
+}
+
+export default App;
